94c8d928487cec95699cc39cfae628f02e20a037
notyou <101701422+a8sh@users.noreply.github.com>
2023-08-04 12:54:59 +0530

    sidebar error removing

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,14 +8,18 @@ import Sidebar from './sidebar';
 
 
 
-const MobileSidebar = () => {
+const MobileSidebar = ({
+    isPro
+}:{
+    isPro: boolean;
+}) => {
     return ( 
         <Sheet>
             <SheetTrigger className='md:hidden pr-4'>
                 <Menu/>
             </SheetTrigger>
             <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar/>
+                <Sidebar isPro={isPro}/>
             </SheetContent>
         </Sheet>
      );
